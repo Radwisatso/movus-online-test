f168d59447c1103592e7d80a37898baa8c194fe0
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2022-07-24 14:03:46 +0700

    Adding global container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { InnerContainer, OuterContainer } from "./common/Container";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world
-        </p>
-        <p>Testing</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OuterContainer>
+      <InnerContainer>
+        <div>
+          <h1>Bookkeeper! Hello!</h1>
+        </div>
+      </InnerContainer>
+    </OuterContainer>
   );
 }
-
-export default App;
---
+++ src/common/Container.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const OuterContainer = styled.div`
+    margin: 0 200px 0 200px;
+`
+
+export const InnerContainer = styled.div`
+    padding: 0 1em 0 1em;
+`
